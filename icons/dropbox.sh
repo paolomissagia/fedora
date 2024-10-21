7f741b9ae266b3228017210688113d22765dc398
Paolo Missagia <login@paolomissagia.com>
2024-10-21 08:32:41 +0100

    added icons with issue fixed

---
+++ icons/dropbox.sh
@@ -0,0 +1,10 @@
+cat <<EOF >~/.local/share/applications/dropbox.desktop
+[Desktop Entry]
+Name=Dropbox
+Comment=Open Dropbox Folder
+Exec=nautilus Dropbox
+Icon=dropbox
+Terminal=false
+Type=Application
+Categories=Network;FileTransfer;
+EOF
