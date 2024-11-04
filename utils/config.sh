269b99fa8567c4239af6fb7c8206817b17ade082
Paolo Missagia <login@paolomissagia.com>
2024-11-04 21:27:04 +0000

    added more config

--- utils/config.sh
+++ utils/config.sh
@@ -1,5 +1,8 @@
-# display
-gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing rgba
+# disable extenion
+gnome-extensions disable background-logo@fedorahosted.org
+
+# improve font
+gsettings set org.gnome.desktop.interface font-antialiasing 'rgba'
 
 # show battery level
 gsettings set org.gnome.desktop.interface show-battery-percentage true
@@ -13,3 +16,8 @@ gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 2
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+
+# multitasking
+gsettings set org.gnome.desktop.interface enable-hot-corners false
+gsettings set org.gnome.mutter edge-tiling false
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
