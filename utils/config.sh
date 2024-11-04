5c2569c1e10bc8062a46f4674fdc4f3611cbe7b8
Paolo Missagia <login@paolomissagia.com>
2024-11-04 09:28:59 +0000

    Update config.sh

--- utils/config.sh
+++ utils/config.sh
@@ -1,16 +1,8 @@
 # extensions
 gnome-extensions disable background-logo@fedorahosted.org
 
-# fractional scaling
-gsettings set org.gnome.mutter experimental-features '["scale-monitor-framebuffer", "xwayland-native-scaling"]'
-gsettings set org.gnome.desktop.interface scaling-factor 1.5
-
 # display
 gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing rgba
-gsettings set org.gnome.settings-daemon.plugins.xsettings hinting slight
-
-# auto brightness
-gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 
 # show battery level
 gsettings set org.gnome.desktop.interface show-battery-percentage true
