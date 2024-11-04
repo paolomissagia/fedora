da16bee3f09fde057307d824c20604f718af7849
Paolo Missagia <login@paolomissagia.com>
2024-11-04 20:39:35 +0000

    moved libraries file

---
+++ libraries.sh
@@ -0,0 +1,3 @@
+sudo dnf group install -y "Development Tools"
+
+sudo dnf install -y curl stow dnf-plugins-core
--- utils/libraries.sh
+++
@@ -1,3 +0,0 @@
-dnf group install -y "Development Tools"
-
-sudo dnf install -y git curl stow dnf-plugins-core
