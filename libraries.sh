2cdac79087dab52f3fe94ecfa8212e3987f451c2
Paolo Missagia <login@paolomissagia.com>
2024-11-05 08:28:40 +0000

    fix group install and ruby libraries

--- libraries.sh
+++ libraries.sh
@@ -1,3 +1,8 @@
-sudo dnf group install -y "Development Tools"
+sudo dnf group install -y development-tools
 
 sudo dnf install -y curl stow dnf-plugins-core
+
+sudo dnf install -y \
+    git-core gcc rust patch make bzip2 openssl-devel libyaml-devel \
+    libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel \
+    perl-FindBin perl-lib perl-File-Compare
